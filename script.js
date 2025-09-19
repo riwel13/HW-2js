536d32125f167b3f0fe85295b441a6f4832a4f0a
riwel13 <karina.velmushkina@gmail.com>
2025-09-19 17:24:23 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+
+	
+// (0 °C × 9/5) + 32 = 32 °F
+
+
+let celsius = 15;
+let fahrenheit = (celsius * 9/5) + 32;
+console.log(fahrenheit);
+
+
+let daysInMonth = 30;
+let hoursInMonth = daysInMonth * 24;
+let minutesInMonth = hoursInMonth * 60;
+console.log(hoursInMonth, minutesInMonth);
+
+
+let health = 100;
+let energy = 100;
+energy -= 50;
+health -= 50;
+console.log(health, energy);
+
+    
+
+let totalPrice = 200;
+let discount = 0.1;
+let discountedPrice = (1 - discount) * totalPrice;
+console.log(
+    discountedPrice
+);
+
+const  loatNumber = 12.78
+const roundedDown = Math.floor(loatNumber);
+console.log(roundedDown);
+
+const floatString = "45.67";
+const parsedFloat = parseFloat(floatString)
+console.log(
+    parsedFloat
+);
+
+const intString = "123";
+const parsedInt = parseInt(intString);
+
+const number = 25;
+const sqrtNumber = Math.sqrt(number);
+console.log(sqrtNumber);
+
+const integer = 42
+const stringNumber = "256"
+const convertedInt = parseInt(stringNumber) 
+console.log(convertedInt, integer);
+
+const convertedString = integer.toString()
+console.log(convertedString, stringNumber);
+
+
+
+
